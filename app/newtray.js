adb9f84738650bb138d56279aeddc1aec09f3875
Alicia Wilkerson <matalina@gmail.com>
2019-12-12 05:47:58 -0600

    Update newtray.js

--- app/newtray.js
+++ app/newtray.js
@@ -4,9 +4,30 @@ const path = require('path');
 const { Tray } = electron;
 
 class NewTray extends Tray {
-  constructor(path) {
+  constructor(path, mainWindow) {
     super(path);
+    this.mainWindow = mainWindow;
+    this.on('click', this.onClick);
   } 
+  
+  onClick(event, bounds) {
+    const { x, y } = bounds;
+    const { height, width } = this.mainWindow.getBounds();
+
+    const yPosition = process.platform === 'darwin': y ? y - height;
+    if(this.mainWindow.isVisible()) {
+        this.mainWindow.hide();
+    }
+    else {
+        this.mainWindow.setBounds({
+            x: x - width / 2,
+            y: y ,
+            height,
+            width,
+        });
+        this.mainWindow.show();
+    }
+  }
 };
 
 moduels.export = NewTray;
